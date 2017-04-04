90606dfae76225e1136df7326e8b4a740c4c3a48
kiple <kiple@mallenom.ru>
2017-04-04 23:56:04 +0300

    initial

---
+++ src/mallenom.superalg.prj/SuperAlg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+using Mallenom.Imaging;
+
+namespace Mallenom.Super
+{
+	public class SuperAlg
+	{
+		public byte Value { set; get; } = 0;
+
+		public int Process(Matrix matrix)
+		{
+			return matrix.Data.Count(v => v == Value);
+		}
+	}
+}
---
+++ src/tests/mallenom.superalg.tests.prj/SuperAlgTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Mallenom.Imaging;
+
+using NUnit.Framework;
+
+// ReSharper disable InconsistentNaming
+namespace Mallenom.Super.Tests
+{
+	[TestFixture]
+	class SuperAlgTests : AssertionHelper
+	{
+		[Test]
+		public void Test()
+		{
+			var alg = new SuperAlg();
+			var matrix = new Matrix(100, 200);
+
+			var result = alg.Process(matrix);
+
+			Expect(() => result, Is.EqualTo(20000));
+		}
+	}
+}
+// ReSharper restore InconsistentNaming
+
